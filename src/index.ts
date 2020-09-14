a7a7641e87de5e7a039399976427f14cc8aac683
Henrik <henrik@thoroee.com>
2020-09-14 20:53:44 +0200

    Add simple driver code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, World!")
+import Neuron from "./Neuron";
+
+const neuron = new Neuron(2)
+
+const data: [number[], number][] = [
+    [[1, 0], 1],
+    [[0, 1], 0],
+    [[1, 1], 1],
+    [[0, 0], 0]
+]
+
+for (let i = 0; i < 1000000; ++i) {
+    data.forEach(d => {
+        neuron.train(d[0], d[1])
+    })
+}
+
+data.forEach(d => {
+    console.log(neuron.feedforward(d[0]))
+})
