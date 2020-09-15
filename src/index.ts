f5ff97a0f61d2db0bada4b186caa4be361ef6718
Henrik <henrik@thoroee.com>
2020-09-15 22:21:50 +0200

    Add network driver

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
+import NeuralNetwork from "./NeuralNetwork";
 import Neuron from "./Neuron";
 
 const neuron = new Neuron(2)
 
+const net = new NeuralNetwork()
+
 const data: [number[], number][] = [
-    [[1, 0], 1],
-    [[0, 1], 0],
-    [[1, 1], 1],
-    [[0, 0], 0]
+    [[2, 0], 0],
+    [[0, 2], 1],
+    [[2, 2], 1],
+    [[0, 0], 0],
+    [[1, 0], 1]
 ]
+
+// for (let i = 0; i < 1000000; ++i) {
+//     data.forEach(d => {
+//         net.train(d[0], [d[1]])
+//     })
+// } 
+
+// data.forEach(d => {
+//     console.log(net.feedforward(d[0]))
+// })
 
 for (let i = 0; i < 1000000; ++i) {
     data.forEach(d => {
